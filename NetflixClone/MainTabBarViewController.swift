b29ad899f68492dd3b215cdead44bff6d7e9f9a6
devanshibhatia29 <devanshi.bhatia@salesforce.com>
2025-08-14 11:41:28 +0530

    Claude AI Fix: Update NetflixClone/MainTabBarViewController.swift

--- NetflixClone/MainTabBarViewController.swift
+++ NetflixClone/MainTabBarViewController.swift
@@ -1,37 +1,50 @@
-//
-//  ViewController.swift
-//  NetflixClone
-//
-//  Created by Vasu Chand on 16/10/24.
-//
-
 import UIKit
 
 class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let vc1 = UINavigationController(rootViewController: HomeViewController())
-        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
-        let vc3 = UINavigationController(rootViewController: SearchViewController())
-        let vc4 = UINavigationController(rootViewController: DownloadViewController())
-        
-        vc1.tabBarItem.image = UIImage(systemName: "hosue")
-        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
-        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
-        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
-        
-        vc1.title = "Netflix"
-        vc2.title = "Incoming"
-        vc3.title = "Top Search"
-        vc4.title = "Downloads"
-        
-        tabBar.tintColor = .label
-        
-        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+        setupTabBar()
+        setupViewControllers()
     }
-
-
+    
+    private func setupTabBar() {
+        tabBar.tintColor = .label
+        tabBar.backgroundColor = .systemBackground
+        tabBar.isTranslucent = false
+    }
+    
+    private func setupViewControllers() {
+        let homeVC = UINavigationController(rootViewController: HomeViewController())
+        let searchVC = UINavigationController(rootViewController: SearchViewController())
+        let upcomingVC = UINavigationController(rootViewController: UpcomingViewController())
+        let downloadVC = UINavigationController(rootViewController: DownloadViewController())
+        
+        // Set tab bar items with icons
+        homeVC.tabBarItem = UITabBarItem(
+            title: "Home",
+            image: UIImage(systemName: "house"),
+            selectedImage: UIImage(systemName: "house.fill")
+        )
+        
+        searchVC.tabBarItem = UITabBarItem(
+            title: "Search",
+            image: UIImage(systemName: "magnifyingglass"),
+            selectedImage: UIImage(systemName: "magnifyingglass")
+        )
+        
+        upcomingVC.tabBarItem = UITabBarItem(
+            title: "Coming Soon",
+            image: UIImage(systemName: "play.circle"),
+            selectedImage: UIImage(systemName: "play.circle.fill")
+        )
+        
+        downloadVC.tabBarItem = UITabBarItem(
+            title: "Downloads",
+            image: UIImage(systemName: "arrow.down.to.line"),
+            selectedImage: UIImage(systemName: "arrow.down.to.line.compact")
+        )
+        
+        setViewControllers([homeVC, searchVC, upcomingVC, downloadVC], animated: false)
+    }
 }
-
